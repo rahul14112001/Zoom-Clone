4f965b86ddd922db95bdba2b2d39f20e399738f6
rahul14112001 <110318129+rahul14112001@users.noreply.github.com>
2023-09-24 17:38:31 +0530

    Delete react-zoom-clone directory

--- react-zoom-clone/src/app/hooks.ts
+++
@@ -1,7 +0,0 @@
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import { TypedUseSelectorHook } from "react-redux";
-import { AppDispatch, RootState } from "./store";
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- react-zoom-clone/src/components/Themes/DarkTheme.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "@elastic/eui/dist/eui_theme_dark.css";
-
-export default function DarkTheme() {
-  return <></>;
-}
